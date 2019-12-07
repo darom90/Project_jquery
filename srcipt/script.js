fe7bbe03b113b0ef429d9cf5ebb3afb295687ee8
darom chhuon <darom.chhuon@student.passerellesnumeriques.org>
2019-12-07 09:55:44 +0700

    add

--- srcipt/script.js
+++ srcipt/script.js
@@ -1,62 +1,94 @@
-var url = "https://raw.githubusercontent.com/ronanogor/jquery-project/master/database-v1.json";
-$(document).ready(() => {
-    $('#recipe').on('change', () => {
+
+$(document).ready(function () {
+    $('#recipe').on('change', function () {
         var recipes = $('#recipe').val();
         chooseRecipe(recipes);
     });
 });
-var getAPI = (api) => {
-    $.ajax({
-        dataType: 'json',
-        url: api,
-        success: (data) => getRecipes(data),
-        error: () => console.error("Cannot request data")
-    });
-}
-
-
-function getRecipes(datas) {
-
-    datas.recipes.forEach(recs => {
-
-        getIngrediant(recs); // get all ingrediant
-
-    });
-}
-
-function getIngrediant(recipe) {
-    recipe.ingredients.forEach(element => {
-        showIngrediantTable(element);
-    })
-}
-var showIngrediantTable = (element) => {
-
-    var ingrediant = "";
-    ingrediant += `
-        <tr>
-            <td><img src="${element.iconUrl}" width="70" class="img-fluid circle-rounded"></td>
-            <td>${element.quantity}</td>
-            <td>${element.unit[0]}</td>
-            <td>${element.name}</td>
-        </tr>
-    `;
-    $('#result').append(ingrediant);
-}
 var chooseRecipe = (myRecipe) => {
-    var data = parseInt(myRecipe);
-    switch (data) {
-        case 2:
-            getAPI(url);
-            hideAlert();
-            break;
-        case 3:
-            data();
-            break;
+    var darom = parseInt(myRecipe);
+    switch (darom) {
         case 1:
             data();
+
             break;
-        default: console.warn("do not something");
+        case 2:
+            datas();
+            break;
     }
 }
+function data() {
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        success: function (datas) {
+            var result = "";
+           
+            datas.recipes.forEach(element => {
 
+                if (element.id == 0) {
+                    result += `
+                    ${element.name}
+                         <img src="${element.iconUrl}" width="90" class="img-fluid circle-rounded">
+                    `;
+                }
+            });
+            $('#results').html(result);
+            var ingrediant = "";
+            datas.recipes.forEach(element => {
+                element.ingredients.forEach(item => {
+                    if (element.id == 0) {
+                        ingrediant += `
+                                <tr>
+                                    ${element.name}
+                                    <td><img src="${item.iconUrl}" width="70" class="img-fluid circle-rounded"></td>
+                                    <td>${item.quantity}</td>
+                                    <td>${item.unit[0]}</td>
+                                    <td>${item.name}</td>
+                                </tr>
+                        `;
+                    }
+                    $('#result').html(ingrediant);
+                })
+            });
+        }
+    });
+}
+function datas() {
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        success: function (datas) {
+            var result = "";
+            datas.recipes.forEach(element => {
 
+                if (element.id == 1) {
+                    result += `
+                            ${element.name}
+                           <img src="${element.iconUrl}" width="90" class="img-fluid circle-rounded">        
+                    `;
+                }
+            });
+            $('#results').html(result);
+            var ingrediant = "";
+            datas.recipes.forEach(element => {
+                element.ingredients.forEach(item => {
+                    if (element.id == 1) {
+                        ingrediant += `
+                                <tr>
+                                    ${element.name}
+                                    <td><img src="${item.iconUrl}" width="70" class="img-fluid circle-rounded"></td>
+                                    <td>${item.quantity}</td>
+                                    <td>${item.unit[0]}</td>
+                                    <td>${item.name}</td>
+                                </tr>
+                        `;
+                    }
+                    $('#result').html(ingrediant);
+                })
+            });
+        }
+    });
+}
