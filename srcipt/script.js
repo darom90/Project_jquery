590f7dfc8972c3339652dcaccb75ce4bb34dc6c3
darom chhuon <darom.chhuon@student.passerellesnumeriques.org>
2019-12-16 18:42:22 +0700

    update html and script

--- srcipt/script.js
+++ srcipt/script.js
@@ -3,6 +3,8 @@ function getUrl() {
     return url;
 }
 $(document).ready(function () {
+    $('#ruler').hide();
+    $('#show').hide();
     requestApi();
 });
 
@@ -32,24 +34,28 @@ function chooseRecipe(recip) {
 var Recipes = (data) => {
     data.forEach(element => {
         $('#recipes').on('change', function () {
+            $('#ruler').show();
+            $('#show').show();
             var result = $('#recipes').val();
             if (element.id == result) {
                 getRecipt(element);
                 getIngredient(element);
+                getInstruction(element.instructions);
             }
         });
     });
 }
+// get recipt
 var getRecipt = (getOut) => {
     var getOutput = "";
-    getOutput +=`
+    getOutput += `
         ${getOut.name}
         <img src="${getOut.iconUrl}" class="img-fluid rounded" width="150">
     `
     $("#result").html(getOutput);
 }
 
-
+// get ingredient
 var Ingredient = (data) => {
     data.forEach(element => {
         $('#recipes').on('change', function () {
@@ -60,18 +66,12 @@ var Ingredient = (data) => {
         });
     });
 }
-var get= (getOut) => {
-    var Ingredient = "";
-    Ingredient +=`
-        ${getOut.instructions}
-    `
-    $("#recip").html(Ingredient);
-}
 
-var getIngredient = (getdata) =>{
-    var display ="";
-        getdata.ingredients.forEach(element =>{
-            display +=`
+// get ingredient
+var getIngredient = (getdata) => {
+    var display = "";
+    getdata.ingredients.forEach(element => {
+        display += `
                 <tr>
                 <td><img src="${element.iconUrl}" class="img-fluid rounded" width="80"><td>
                     <td>${element.quantity}</td>
@@ -79,9 +79,54 @@ var getIngredient = (getdata) =>{
                     <td>${element.name}</td>
                 </tr>
             `;
-        }); 
-        $('#ingredient').html(display);
+    });
+    $('#ingredient').html(display);
+    $('#ingredients').html('Ingredient');
 }
+
+// get instruction
+function getInstruction(insturc) {
+    var result = "";
+    var get = insturc.split('<step>');
+    for (let i = 1; i < get.length; i++) {
+        result += `
+            <h5 class="text-secondary">Step:${i}</h5>
+            <p>${get[i]}</p>
+        `;
+    }
+    $('#instruction').html(result);
+    $('#instructions').html('Instruction');
+}
+
+$(document).ready(function () {
+    $('#minus').on('click', function () {
+        var members = $('#member').val();
+        decreaseMember(members);
+    });
+    $('#add').on('click', function () {
+        var members = $('#member').val();
+        increaseMember(members);
+    });
+});
+$('#number').html('Number Of Person');
+// ninus number
+function decreaseMember(minus) {
+    var member = parseInt(minus) - 1;
+    if (member >= 0) {
+        $('#member').val(member);
+        compute(member);
+    }
+}
+
+// increment number
+function increaseMember(add) {
+    var members = parseInt(add) + 1;
+    if (members <= 15) {
+        $('#member').val(members);
+        compute(members);
+    }
+}
+
 
 
 
